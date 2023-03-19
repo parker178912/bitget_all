0b610c908ac3839931dabfbcfc0ec2fc0d30107d
傳Yee <parker178912@gmail.com>
2023-03-20 00:47:01 +0800

    make it better

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ orderApi = order.OrderApi(api_key, secret_key, passphrase, use_server_time=False
 accountApi = accounts.AccountApi(api_key, secret_key, passphrase, use_server_time=False, first=False)
 traceApi = trace.TraceApi(api_key, secret_key, passphrase, use_server_time=False, first=False)
 
-def open(symbol, size, side):
+def open(symbol, size, side, stoplossprice):
     try:
         print(f"sending order - {side} {size} {symbol}")
-        result = orderApi.place_order(symbol, marginCoin='USDT', size=size, side=side, orderType='market', timeInForceValue='normal')
+        result = orderApi.place_order(symbol, marginCoin='USDT', size=size, side=side, orderType='market', timeInForceValue='normal', presetStopLossPrice=stoplossprice)
         print(result)
     except Exception as e:
         print("an exception occured - {}".format(e))
@@ -56,18 +56,19 @@ def webhook():
     market_position = data['strategy']['market_position']
     pre_market_position = data['strategy']['prev_market_position']
     action = data['strategy']['order_action']
+    stoplossprice = str(int(data['strategy']['stopLossPrice']*100)/100)
     if(market_position == "flat" and action == "buy"): #close short
         close(symbol, "short")
     elif(market_position == "flat" and action == "sell"): #close long
         close(symbol, "long")
     elif(market_position == "long" and action == "buy" and pre_market_position == "flat"): #long entry
-        open(symbol, size, "open_long")
+        open(symbol, size, "open_long", stoplossprice)
         time.sleep(2)
-        open(symbol, size, "open_long")
+        open(symbol, size, "open_long", stoplossprice)
     elif(market_position == "short" and action == "sell" and pre_market_position == "flat"): #short entry
-        open(symbol, size, "open_short")
+        open(symbol, size, "open_short", stoplossprice)
         time.sleep(2)        
-        open(symbol, size, "open_short")
+        open(symbol, size, "open_short", stoplossprice)
     elif(market_position == "long" and action == "sell" and pre_market_position == "long"): #close long1
         close(symbol, "long")
     elif(market_position == "short" and action == "buy" and pre_market_position == "short"): #close short1
@@ -75,15 +76,15 @@ def webhook():
     elif(market_position == "long" and action == "buy" and pre_market_position == "short"): #close short and open long
         close(symbol, "short")
         close(symbol, "short")
-        open(symbol, size, "open_long")
+        open(symbol, size, "open_long", stoplossprice)
         time.sleep(2)
-        open(symbol, size, "open_long")
+        open(symbol, size, "open_long", stoplossprice)
     elif(market_position == "short" and action == "sell" and pre_market_position == "long"): #close long and open short
         close(symbol, "long")
         close(symbol, "long")
-        open(symbol, size, "open_short")
+        open(symbol, size, "open_short", stoplossprice)
         time.sleep(2)
-        open(symbol, size, "open_short")     
+        open(symbol, size, "open_short", stoplossprice)     
     return{
         "code":"success",
         "message":"0117 dsv "
